6b917c1bd53bf17e1978a2fac02ef1f05e75fa5c
Rodrigo de Souza Garcia <72108170+rodrigogarcia1@users.noreply.github.com>
2023-10-25 20:27:52 -0300

    Update and rename teste.txt to papelMoeda_teste.java

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/papelMoeda_teste.java
@@ -0,0 +1,19 @@
+/*
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PapelMoedaTest {
+
+    @Test
+    public void testGetValor() {
+        PapelMoeda papelMoeda = new PapelMoeda(10, 5);
+        assertEquals(10, papelMoeda.getValor());
+    }
+
+    @Test
+    public void testGetQuantidade() {
+        PapelMoeda papelMoeda = new PapelMoeda(10, 5);
+        assertEquals(5, papelMoeda.getQuantidade());
+    }
+}
+*/
