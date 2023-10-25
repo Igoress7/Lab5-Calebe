029b2c2664456ff8b7a5b3aee4f45773bc39cfa2
Rodrigo de Souza Garcia <72108170+rodrigogarcia1@users.noreply.github.com>
2023-10-25 20:29:08 -0300

    Update and rename Source Code Inspection/src/br/calebe/ticketmachine/core/teste3.txt to Source Code Inspection/src/br/calebe/ticketmachine/core/troco_teste*/troco_teste.java

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/troco_teste*/troco_teste.java
@@ -0,0 +1,23 @@
+/*
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.Iterator;
+
+public class TrocoTest {
+
+    @Test
+    public void testTrocoIterator() {
+        Troco troco = new Troco(275);
+        Iterator<PapelMoeda> iterator = troco.getIterator();
+        int count = 0;
+        while (iterator.hasNext()) {
+            PapelMoeda papelMoeda = iterator.next();
+            if (papelMoeda != null) {
+                count++;
+            }
+        }
+        assertEquals(4, count); // ajuste o valor esperado de acordo com o comportamento do Troco
+    }
+}
+
+*/
